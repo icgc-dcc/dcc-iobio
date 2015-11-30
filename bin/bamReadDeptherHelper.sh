04feed987881e4f01069755c2c52af9afc120add
Douglas Wong <dwong@olm-dwong.oicr.on.ca>
2015-11-30 11:38:55 -0500

    Added comment explaining pre-caching of bai file

--- bin/bamReadDeptherHelper.sh
+++ bin/bamReadDeptherHelper.sh
@@ -9,6 +9,9 @@ fi
 
 FULL_NAME=$PATH$input
 
+# We are applying dd to the bai file and redirecting it to /dev/null because we want to
+# cache it to the operating system, before applying cat to it. This is to avoid the many
+# connections that the storage client will have to make, since the buffer is small.
 /bin/dd if=$FULL_NAME bs=1048576 > /dev/null
 cmd="/bin/cat \"$FULL_NAME\" | /home/iobio/iobio/bin/bamReadDepther"
 eval $cmd
