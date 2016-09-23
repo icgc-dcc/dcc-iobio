2390d7042979c79d20537e57e93de4f84e0635a6
andricDu <dusan.andric@oicr.on.ca>
2016-09-23 12:35:32 -0400

    Added scripts used to run iobio on server.

---
+++ scripts/run_iobio_vcf.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+export ACCESSTOKEN=""
+export PUB_HTTP_PORT="8080"
+docker run -p $PUB_HTTP_PORT:80 -e "PUB_HOSTNAME=dcc-iobio" -e "PUB_HTTP_PORT=80" -e ACCESSTOKEN=$ACCESSTOKEN --privileged icgcdcc/dcc-iobio:latest-vcf
